7c2cef6d72d162822393c34688dbe13600fc20e0
Abdul Fahmi Aziz <abdulfahmi78@gmail.com>
2021-11-26 21:44:08 +0700

    coba bikin insert

--- src/index.js
+++ src/index.js
@@ -14,10 +14,35 @@ const parser = require('body-parser');
 const app = express();
 const port = process.env.SERVERPORT || 3000
 
-app.use('/', (req, res)=> {
-    res.send("Hello World")
+app.use('/', (req, res) => {
+    res.send(`Hello world`)
 })
 
-app.listen(port, function () {
+app.listen(port, function() {
     console.log('Server running on port:', port, 'at', new Date())
 })
+
+// username dan password database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'datain.id'
+});
+// end setting database connection
+
+// menerima post dari data form add transactions di lanjutkan melakukan insert ke database
+app.post('/addTransaction', (req, res) => {
+    connection.query(
+        'INSERT INTO data_collections (item_nama, weight, qty, grand_price) VALUES (?,?,?,?)', [req.body.item_nama, req.body.weight, req.body.qty, req.body.grand_price],
+        (error, results) => {
+            if (results.affectedRows) {
+                req.flash('item berhasil di tambahkan ');
+            } else {
+                req.flash('item gagal di tambahkan')
+            }
+            res.redirect('/');
+        }
+    )
+});
+// end insert form post add transactions
